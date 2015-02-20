281282da0bd65ac8f3ec55ba38e6130c426dbad7
heartyoh <heartyoh@gmail.com>
2015-02-20 16:38:09 +0900

    pip for iOS

--- app/assets/javascripts/app/mixin/MediaGroup.js
+++ app/assets/javascripts/app/mixin/MediaGroup.js
@@ -1,54 +1,62 @@
 Ext.define('App.mixin.MediaGroup', function() {
   $.createEventCapturing = (function () {
-      var special = $.event.special;
-      return function (names) {
-          if (!document.addEventListener) {
-              return;
-          }
-          if (typeof names == 'string') {
-              names = [names];
-          }
-          $.each(names, function (i, name) {
-              var handler = function (e) {
-                  e = $.event.fix(e);
+    var special = $.event.special;
+    return function (names) {
+      if (!document.addEventListener) {
+        return;
+      }
+      if (typeof names == 'string') {
+        names = [names];
+      }
+      $.each(names, function (i, name) {
+        var handler = function (e) {
+          e = $.event.fix(e);
 
-                  return $.event.dispatch.call(this, e);
-              };
-              special[name] = special[name] || {};
-              if (special[name].setup || special[name].teardown) {
-                  return;
-              }
-              $.extend(special[name], {
-                  setup: function () {
-                      this.addEventListener(name, handler, true);
-                  },
-                  teardown: function () {
-                      this.removeEventListener(name, handler, true);
-                  }
-              });
-          });
-      };
+          return $.event.dispatch.call(this, e);
+        };
+        special[name] = special[name] || {};
+        if (special[name].setup || special[name].teardown) {
+          return;
+        }
+        $.extend(special[name], {
+          setup: function () {
+            this.addEventListener(name, handler, true);
+          },
+          teardown: function () {
+            this.removeEventListener(name, handler, true);
+          }
+        });
+      });
+    };
   })();
 
   $.createEventCapturing(['play', 'pause', 'seeked']);
 
-  $(document).on('click', 'video:not([controls])', function(e) {
+  $(document).on('click', '.pip-container img.forward-layer', function(){
+    var img = e.target;
+
+    var mediagroup = $(img).attr('xmediagroup');
+    $('img[xmediagroup=' + mediagroup + ']').not(img).each(function(){
+      $(this).removeClass("backward-layer").addClass("forward-layer");
+    });
+
+    $(this).addClass("backward-layer").removeClass("forward-layer");
+  });
+
+  $(document).on('click', '.pip-container video.forward-layer', function(e){
     var video = e.target;
 
     var mediagroup = $(video).attr('xmediagroup');
     $('video[xmediagroup=' + mediagroup + ']').not(video).each(function(){
-      $(this).attr('controls', false);
+      $(this).prop('controls', false);
+      $(this).removeClass("backward-layer").addClass("forward-layer");
     });
 
-    $(video).attr('controls', true);
-  });
-
-  $(document).on('click', '.pip-container *', function(){
-    $( ".backward-layer" ).removeClass("backward-layer").addClass("forward-layer");
+    $(video).prop('controls', true);
     $(this).addClass("backward-layer").removeClass("forward-layer");
   });
 
-  $(document).on('play', 'video[controls]', function(e) {
+  $(document).on('play', 'video.backward-layer', function(e) {
     var video = e.target;
 
     var mediagroup = $(video).attr('xmediagroup');
@@ -58,7 +66,7 @@ Ext.define('App.mixin.MediaGroup', function() {
     });
   });
 
-  $(document).on('pause', 'video[controls]', function(e) {
+  $(document).on('pause', 'video.backward-layer', function(e) {
     var video = e.target;
 
     var mediagroup = $(video).attr('xmediagroup');
@@ -68,7 +76,7 @@ Ext.define('App.mixin.MediaGroup', function() {
     });
   });
 
-  $(document).on('seeked', 'video[controls]', function(e) {
+  $(document).on('seeked', 'video.backward-layer', function(e) {
     var video = e.target;
 
     var mediagroup = $(video).attr('xmediagroup');
